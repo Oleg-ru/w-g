018cab5317d86ef41c3e4ec7dd6710a7dbed693f
Oleg <oleg_09-95@mail.ru>
2026-02-25 20:15:28 +0300

    JS 87 видео Сделан проект "Магазин велосипедов и аксессуаров"

--- practice/project-6/script.js
+++ practice/project-6/script.js
@@ -2,6 +2,9 @@ import {products1} from './data1.js'
 import {products2} from './data2.js'
 
 const rootEl = document.getElementById('root');
+const findInputEl = document.getElementById("find-input");
+const findButtonEl = document.getElementById("find-button");
+const resetButtonEl = document.getElementById("reset-button");
 
 const parseData = (data1, data2) => {
     return [...JSON.parse(data1), ...JSON.parse(data2)];
@@ -41,23 +44,23 @@ const getSale = (products) => {
         return {...product, price: newPrice}
     })
 };
-
 const render = (products) => {
     const containerEl = document.createElement("div");
     rootEl.append(containerEl);
 
-    const resulDOM = products.map(({name, brand, img, price, color, description, weight}) => {
+    const resulDOM = products.map(({name, brand, img, price, color, description, weight, size }) => {
 
         return `
     <div class="product">
         <div class="title">${name}</div>
+       
         <div class="product-data">
             <p class="brend">Производитель: <span class="brend-desk">${brand}</span></p>
-            <img src="${img}" alt="product">
-            <p class="price">Цена: <span class="price-desk">${price}</span></p>
-            <p class="oldPrice">Старая цена: <span class="oldPrice-desc">${0}</span></p>
+            <img class="image" src="${img}" alt="product">
+            <p class="price">Цена: <span class="price-desk">${price} руб.</span></p>
             <p class="color">Цвет: <span class="color-desc">${color}</span></p>
             <p class="description">Описание: <span class="description-desc">${description}</span></p>
+            ${size ? `<p class="size">Размер: <span class="size-desk">${size}</span></p>` : ''}
             <p class="weight">Вес: <span class="weight-desc">${weight}</span></p>
         </div>
     </div>
@@ -65,8 +68,45 @@ const render = (products) => {
     })
     rootEl.innerHTML = resulDOM.join("")
 }
+const findProduct = (products, findName) => {
+    return products.filter((product) => product.name.toUpperCase().includes(findName.toUpperCase()))
+};
+const onFindProductHandle = (products) => {
+    const findName = findInputEl.value;
+    if (findName) {
+        render([]);
+        loaderSwitch(true);
+        setTimeout(() => {
+            loaderSwitch(false);
+            render(findProduct(products, findName));
+        }, 1000)
+    }
+};
+const loaderSwitch = (action) => {
+    if (action) {
+        const loaderEl = document.createElement("div");
+        loaderEl.id = 'loader'
+        loaderEl.classList.add('loader');
+        rootEl.append(loaderEl);
+    } else {
+        const loaderEl = document.querySelector('#loader');
+        loaderEl.remove();
+    }
+};
 
-const products = sortData(parseData(products1, products2), "велосипеды", "аксессуары")
-render(products)
-console.log(getSale(products))
-
+const products = sortData(parseData(products1, products2), "велосипеды", "аксессуары");
+findButtonEl.addEventListener('click', () => {onFindProductHandle(products)});
+findInputEl.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        onFindProductHandle(products)
+    }
+});
+resetButtonEl.addEventListener('click', () => {
+    render(products);
+    findInputEl.value = '';
+})
+loaderSwitch(true)
+setTimeout(() => {
+    loaderSwitch(false);
+    render(products);
+}, 1000)
