fabe38d7d929ca25e792d1f513e3efda4ae287f2
Oleg <oleg_09-95@mail.ru>
2026-02-28 18:49:07 +0300

    React 12 видео Еще примеры обработчиков событий

--- react/my-app/src/pages/HomePage.jsx
+++ react/my-app/src/pages/HomePage.jsx
@@ -8,10 +8,46 @@ function HomePage(props) {
         setText(e.target.value)
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('Форма отправлена. Ждите')
+    };
+
+    function handleMouseOver() {
+        console.log('Навел на div')
+    }
+
+    function handleMouseOut() {
+        console.log('Ушел с div')
+    }
+
+    function handleLoad() {
+        console.log('Изображение загружено')
+    }
+
+    function handleError() {
+        console.log('Ошибка в загрузке изображения!')
+    }
+
     return (
         <div className="homePage">
-            <input onChange={handleOnChange} />
+            <input onChange={handleOnChange}/>
             <div>Текст: {text}</div>
+            <form onSubmit={handleSubmit}>
+
+                <button type="submit">Отправить</button>
+            </form>
+            <div style={{width: "100px", height: "100px", backgroundColor: "green"}}
+                 onMouseOver={handleMouseOver}
+                 onMouseOut={handleMouseOut}
+            >
+                Наведи на меня
+            </div>
+            <img src="https://example.com/image.jpg"
+                 alt="picter"
+                 onLoad={handleLoad}
+                 onError={handleError}
+            />
         </div>
     );
 }
