5c1acf5e351138cf0f21944c7c0dbd0f11242f03
Oleg <oleg_09-95@mail.ru>
2026-02-28 23:07:48 +0300

    JS 111 видео Drag&Drop Делаем тест. Промежуточная фиксация

---
+++ practice/drag_and_drop/02/script.js
@@ -0,0 +1,56 @@
+/*
+Быстрый минимальный старт:
+1️⃣ Для зоны drop для события "dragover" вызвать preventDefault()
+2️⃣ Для Drag элемента для события "drop" вызвать preventDefault(), далее в нужную зону append(элемент) элемент
+ */
+
+const dragItems =
+    document.querySelectorAll("[id^=drag-]"); // выбор по атрибуту (атрибутный селектор) если есть Id с началом drag- берем их
+
+//Выбираем зоны drop
+const dropZones = document.querySelectorAll("[id^=drop-zone-]");
+//----------------------------------------------------------------------------------------------------------------------
+
+//Для каждого элемента добавляем слушатель событие на начало перетаскивания
+dragItems.forEach(item => item.addEventListener('dragstart', dragStart))
+function dragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.id);
+    console.log(event.target.id)
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//Для каждой drop зоны устанавливаем слушатели событий
+//Для drop зон говорим, что они готовы принимать элементы (dragover дословно -> перетащить на себя) ❗без этого не будет работать drop❗
+
+dropZones.forEach(zone => {
+    zone.addEventListener('dragover', dragOver);
+    zone.addEventListener('drop', drop);
+})
+
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text/plain');
+    const dragItem = document.getElementById(id);
+
+    //event.target.append(dragItem); //БАГ📛 можно вложить элементы друг в друга с таким способом
+    event.currentTarget.append(dragItem)
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+document.getElementById("check-answers").addEventListener("click", () => {
+
+    let score = 0;
+
+    const correctAnswers = {
+        "zone-1": "drag-1",
+        "zone-2": "drag-2",
+    };
+
+
+})
