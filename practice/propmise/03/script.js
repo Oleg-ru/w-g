dddbaedfadc82b682bdaeb2aaa172f17b56a41a0
Oleg <oleg_09-95@mail.ru>
2026-03-05 21:56:53 +0300

    JS 130 видео Promise. CRUD на примере to-do На sweetalert2 реализовано модальное окно с изменением текста задачи

--- practice/propmise/03/script.js
+++ practice/propmise/03/script.js
@@ -115,13 +115,28 @@ function renderData(tasks) {
 }
 
 async function setNewTextTask(taskId, taskText) {
-    const newText = prompt('Введите новый текст задачи', taskText);
+
+    const {value: newText} = await Swal.fire({
+        title: 'Редактирование задачи',
+        input: 'text',
+        inputLabel: 'ВВедите текст новой задачи',
+        inputValue: taskText,
+        showCancelButton: true,
+        confirmButtonText: 'Сохранить',
+        cancelButtonText: 'Отменить',
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Поле не может быть пустым'
+            }
+        }
+    });
+
     if (newText) {
         try {
             await updateTaskText(taskId, newText);
             await loadData();
         } catch (error) {
-            console.error(error.message)
+            console.error(error.message);
         }
     }
 }
