645578fd783f01e9ca6a9c24d23ef0a99fb70670
Oleg <oleg_09-95@mail.ru>
2026-03-06 20:58:17 +0300

    JS 131 видео Promise. CRUD на примере to-do feature renderData

--- practice/propmise/03/src/app.js
+++ practice/propmise/03/src/app.js
@@ -2,7 +2,7 @@ import {
     getTodos,
 } from "./api/index.js";
 import {hideLoader, showError, showLoader} from "./utils/helpers.js";
-import {initDragAndDrop, initDeleteCompleted, initAddTask, setNewTextTask, initLoadData, initChangeCompleted, initDeleteTask, initChangeTextTask} from "./components/index.js";
+import {initDeleteCompleted, initAddTask, initLoadData, renderData} from "./components/index.js";
 
 const container = document.getElementById("posts-container");
 const taskInput = document.getElementById("task-input");
@@ -14,7 +14,7 @@ export async function loadData() {
     try {
         showLoader()
         const todos = await getTodos();
-        renderData(todos)
+        renderData(todos, container, deleteCompletedButton, downloadButton);
     } catch (error) {
         console.error("Ошибка: " + error.message);
 
@@ -26,71 +26,6 @@ export async function loadData() {
     } finally {
         hideLoader();
     }
-}
-
-function renderData(tasks) {
-    container.innerHTML = '';
-
-    //Если есть хоть 1 выполненная задача показываем кнопку удаления выполненных задач
-    const hasCompletedTasks = tasks.some(task => task.completed);
-    deleteCompletedButton.style.display = hasCompletedTasks ? 'block' : 'none';
-
-    tasks.forEach(task => {
-        const date = new Date(task.createdAt).toLocaleString("ru-RU", {
-            year: "numeric",
-            month: "numeric",
-            day: "numeric",
-            hour: "2-digit",
-            minute: "2-digit"
-        });
-
-        const todoEl = document.createElement('div');
-        todoEl.className = 'todo';
-        todoEl.setAttribute('data-id', task.id);
-
-        //Чекбокс таска
-        const inputEl = document.createElement('input');
-        inputEl.type = 'checkbox';
-        inputEl.checked = task.completed;
-        initChangeCompleted(inputEl, task);
-
-        //Текст таска
-        const taskTextEl = document.createElement('p');
-        taskTextEl.className = task.completed ? 'task-completed' : '';
-        taskTextEl.textContent = task.text;
-
-        //Дата таска
-        const createdDateTaskEl = document.createElement('p');
-        createdDateTaskEl.textContent = date;
-
-        //Кнопка удаления
-        const deleteButtonEl = document.createElement('button');
-        deleteButtonEl.className = 'button-function';
-        initDeleteTask(deleteButtonEl, task)
-        const imgDeleteEl = document.createElement('img');
-        imgDeleteEl.alt = 'Удалить';
-        imgDeleteEl.title = 'Удалить';
-        imgDeleteEl.src = './assets/icons/icon-delete.png';
-        deleteButtonEl.appendChild(imgDeleteEl);
-
-        //Кнопка изменить
-        const setNewTextButtonEl = document.createElement('button');
-        setNewTextButtonEl.className = 'button-function';
-        initChangeTextTask(setNewTextButtonEl, task);
-        const imgSetNewTextEl = document.createElement('img');
-        imgSetNewTextEl.alt = 'Изменить';
-        imgSetNewTextEl.title = 'Изменить';
-        imgSetNewTextEl.src = './assets/icons/icon-update.png';
-        setNewTextButtonEl.appendChild(imgSetNewTextEl);
-
-        //drag&drop
-        initDragAndDrop(todoEl, task, container);
-
-        //Монтируем в контейнер таску
-        todoEl.append(inputEl, taskTextEl, createdDateTaskEl, deleteButtonEl, setNewTextButtonEl);
-        container.append(todoEl);
-    });
-    downloadButton.hidden = true;
 }
 
 initDeleteCompleted(container);
---
+++ practice/propmise/03/src/components/ui/renderData.js
@@ -0,0 +1,66 @@
+import {initChangeCompleted, initChangeTextTask, initDeleteTask, initDragAndDrop} from "../index.js";
+
+export function renderData(tasks, container, deleteCompletedButton, downloadButton) {
+    container.innerHTML = '';
+
+    //Если есть хоть 1 выполненная задача показываем кнопку удаления выполненных задач
+    const hasCompletedTasks = tasks.some(task => task.completed);
+    deleteCompletedButton.style.display = hasCompletedTasks ? 'block' : 'none';
+
+    tasks.forEach(task => {
+        const date = new Date(task.createdAt).toLocaleString("ru-RU", {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit"
+        });
+
+        const todoEl = document.createElement('div');
+        todoEl.className = 'todo';
+        todoEl.setAttribute('data-id', task.id);
+
+        //Чекбокс таска
+        const inputEl = document.createElement('input');
+        inputEl.type = 'checkbox';
+        inputEl.checked = task.completed;
+        initChangeCompleted(inputEl, task);
+
+        //Текст таска
+        const taskTextEl = document.createElement('p');
+        taskTextEl.className = task.completed ? 'task-completed' : '';
+        taskTextEl.textContent = task.text;
+
+        //Дата таска
+        const createdDateTaskEl = document.createElement('p');
+        createdDateTaskEl.textContent = date;
+
+        //Кнопка удаления
+        const deleteButtonEl = document.createElement('button');
+        deleteButtonEl.className = 'button-function';
+        initDeleteTask(deleteButtonEl, task)
+        const imgDeleteEl = document.createElement('img');
+        imgDeleteEl.alt = 'Удалить';
+        imgDeleteEl.title = 'Удалить';
+        imgDeleteEl.src = './assets/icons/icon-delete.png';
+        deleteButtonEl.appendChild(imgDeleteEl);
+
+        //Кнопка изменить
+        const setNewTextButtonEl = document.createElement('button');
+        setNewTextButtonEl.className = 'button-function';
+        initChangeTextTask(setNewTextButtonEl, task);
+        const imgSetNewTextEl = document.createElement('img');
+        imgSetNewTextEl.alt = 'Изменить';
+        imgSetNewTextEl.title = 'Изменить';
+        imgSetNewTextEl.src = './assets/icons/icon-update.png';
+        setNewTextButtonEl.appendChild(imgSetNewTextEl);
+
+        //drag&drop
+        initDragAndDrop(todoEl, task, container);
+
+        //Монтируем в контейнер таску
+        todoEl.append(inputEl, taskTextEl, createdDateTaskEl, deleteButtonEl, setNewTextButtonEl);
+        container.append(todoEl);
+    });
+    downloadButton.hidden = true;
+}
