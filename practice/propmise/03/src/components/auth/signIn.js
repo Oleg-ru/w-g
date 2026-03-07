56e415a6bdaf1542ad28efef0fbbd6572e515137
Oleg <oleg_09-95@mail.ru>
2026-03-07 15:49:43 +0300

    JS 133 видео Promise. CRUD на примере to-do Firebase feature Sign in

---
+++ practice/propmise/03/src/components/auth/signIn.js
@@ -0,0 +1,21 @@
+import {auth, signInWithEmailAndPassword} from "../../firebaseConfig.js";
+
+const signInForm = document.getElementById('signin-form');
+
+signInForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('signin-email').value;
+    const password = document.getElementById('signin-password').value;
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+        console.log("Пользователь успешно авторизован: " + user.uid);
+
+        alert('Авторизация прошла успешно')
+    } catch (error) {
+        console.error('Ошибка авторизации: ', error.message, error.code);
+        alert(`Ошибка авторизации: ${error.message}`);
+    }
+})
