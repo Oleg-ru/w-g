ae7e3a548ec2996c0f28bd967aadaa0560356e9f
Oleg <oleg_09-95@mail.ru>
2026-03-09 16:57:26 +0300

    JS 137 видео Web meteo app Feature: Геолокация (geoLocation.js)

--- practice/propmise/04-web-meteo/init.js
+++ practice/propmise/04-web-meteo/init.js
@@ -1,9 +1,11 @@
 import {switchTheme} from "./src/components/switchTheme.js";
 import {getWeatherByForm} from "./src/components/inputForm.js";
 import {renderCurrentTime} from "./src/helpers/currentTime.js";
+import {geolocation} from "./src/components/geolocation.js";
 
 export function initApp() {
     switchTheme();
     getWeatherByForm();
     renderCurrentTime();
+    geolocation();
 }
