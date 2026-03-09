d5c0f3599cb5a4b1088a0a7ed1b5bfc3229f1659
Oleg <oleg_09-95@mail.ru>
2026-03-09 12:00:50 +0300

    JS 137 видео Web meteo app Feature: Заглавная буква в названии (cityAbbreviaion.js / replaceAbbreviation)

--- src/api/geoData.js
+++ src/api/geoData.js
@@ -1,14 +1,17 @@
 import {apiKey, BASE_URL} from './apiKeyAndHost.js'
 import {showError} from "../components/error.js";
 import {isCyrillic} from "../helpers/checkCyrillic.js";
+import {replaceAbbreviation} from "../helpers/cityAbbreviation.js";
 
 export const getGeoData = async (cityInput) => {
-    const city = cityInput.value.trim();
+    let city = cityInput.value.trim();
     
     if (!city || !isCyrillic(city)) {
         showError('Проверьте название города');
         return;
     }
+
+    city = replaceAbbreviation(city);
     
     try {
         const geoUrl = `${BASE_URL}/geo/1.0/direct`;
