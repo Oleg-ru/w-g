72ccef415d1dffa00dbb6275274ea4c985aa9ec0
Oleg <oleg_09-95@mail.ru>
2026-03-09 17:03:05 +0300

    JS 137 видео Web meteo app Feature: Кнопка "Листать вверх" (scrollToTop.js)

--- practice/propmise/04-web-meteo/init.js
+++ practice/propmise/04-web-meteo/init.js
@@ -2,10 +2,12 @@ import {switchTheme} from "./src/components/switchTheme.js";
 import {getWeatherByForm} from "./src/components/inputForm.js";
 import {renderCurrentTime} from "./src/helpers/currentTime.js";
 import {geolocation} from "./src/components/geolocation.js";
+import {scrollToTop} from "./src/components/scrollToTop.js";
 
 export function initApp() {
     switchTheme();
     getWeatherByForm();
     renderCurrentTime();
     geolocation();
+    scrollToTop();
 }
