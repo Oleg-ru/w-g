7000655544fa586b08f419f38cfa15128b73d780
Oleg <oleg_09-95@mail.ru>
2026-03-23 22:06:53 +0300

    React 51 Реализована и стилизован функционал фильтрации задач

--- react/03-tailwind-todo-app/src/components/MainContent/MainContent.jsx
+++ react/03-tailwind-todo-app/src/components/MainContent/MainContent.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from "../TodoList/TodoList.jsx";
 import AddTodo from "../AddTodo/AddTodo.jsx";
 import Header from "../Header/Header.jsx";
+import TodoFilter from "../TodoFilter/TodoFilter.jsx";
 
 function MainContent({onAdd, todos, setDeletingId, toggleComplete, handleUpdate}) {
+
+    const [filter, setFilter] = useState('all');
+
+    const filteredTodos = todos.filter(todo => {
+        if (filter === 'completed') {
+            return todo.completed
+        }
+        if (filter === 'active') {
+            return !todo.completed
+        }
+        return true;
+    })
+
     return (
         <div className="mx-auto flex flex-col gap-3">
             <Header/>
             <AddTodo onAdd={onAdd}/>
-            <TodoList todos={todos}
+            <TodoFilter filter={filter} setFilter={setFilter}/>
+            <TodoList todos={filteredTodos}
                       setDeletingId={setDeletingId}
                       toggleComplete={toggleComplete}
                       handleUpdate={handleUpdate}
---
+++ react/03-tailwind-todo-app/src/components/TodoFilter/TodoFilter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function TodoFilter({filter, setFilter}) {
+    
+    function buttonClasses(currentFilter) {
+        return `px-4 py-2 rounded transition-color cursor-pointer ${filter === currentFilter 
+            ? 'bg-blue-500 text-white' 
+            : 'bg-blue-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-600'}`
+    }
+    
+    return (
+        <div className="flex flex-col gap-2 md-4">
+            <button onClick={() => {setFilter('all')}} className={buttonClasses('all')}>Все</button>
+            <button onClick={() => {setFilter('active')}} className={buttonClasses('active')}>Не выполненные</button>
+            <button onClick={() => {setFilter('completed')}} className={buttonClasses('completed')}>Выполненные</button>
+        </div>
+    );
+}
+
+export default TodoFilter;
