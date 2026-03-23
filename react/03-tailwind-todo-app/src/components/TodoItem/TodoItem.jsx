b1b838bba1ccf86f687956b6ba23bc21f24568d1
Oleg <oleg_09-95@mail.ru>
2026-03-23 22:34:13 +0300

    React 51 Вынес изменение задачи в компонент

--- react/03-tailwind-todo-app/src/components/TodoItem/TodoItem.jsx
+++ react/03-tailwind-todo-app/src/components/TodoItem/TodoItem.jsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 import CheckboxButton from "../CheckboxButton/CheckboxButton.jsx";
+import TodoEditForm from "../TodoEditForm/TodoEditForm.jsx";
 
 function TodoItem({id, text, onDelete, onToggleComplete, completed, deadline, createdAt, onUpdate}) {
     const [isEditing, setIsEditing] = useState(false);
@@ -42,28 +43,13 @@ function TodoItem({id, text, onDelete, onToggleComplete, completed, deadline, cr
                 <CheckboxButton handleToggle={handleToggle} completed={completed}/>
                 {isEditing
                     ? (
-                        <div className="flex flex-col w-full gap-2 items-stretch" ref={editFormRef}>
-                            <input className="w-full px-2 py-1 border-2 border-blue-500 rounded text-sm text-gray-700 dark:text-gray-300"
-                                   type="text"
-                                   value={editText}
-                                   onChange={(e) => setEditText(e.target.value)}
-                                   onKeyDown={(e) => e.key === 'Enter' && handleSave()}
-                            />
-                            <div className="flex flex-col xs:flex-row gap-2 w-full">
-                                <input className="w-full sm:flex-1 px-2 py-1 border-2 border-blue-500 rounded text-sm text-gray-700 dark:text-gray-300"
-                                       type="datetime-local"
-                                       value={editDeadline}
-                                       onChange={(e) => setEditDeadline(e.target.value)}
-                                />
-                                <button  className="flex items-center justify-center gap-1 px-2 py-1 sn:px-3 sm:py-1 cursor-pointer bg-white border-2 border-green-500 rounded hover:bg-green-50 transition-colors text-sm sm:text-base"
-                                         onClick={handleSave}
-                                >
-                                    ✔
-                                    <span className="sm:hidden">OK</span>
-                                    <span className="hidden sm:inline">Готово</span>
-                                </button>
-                            </div>
-                        </div>
+                        <TodoEditForm editText={editText}
+                                      setEditText={setEditText}
+                                      editDeadline={editDeadline}
+                                      setEditDeadline={setEditDeadline}
+                                      innerRef={editFormRef}
+                                      handleSave={handleSave}
+                        />
                     )
                     : (
                         <div className="flex flex-col cursor-pointer"
