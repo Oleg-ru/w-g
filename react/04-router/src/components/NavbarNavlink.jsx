c92b9d5f3846cfb7d74be6821734dcc5f092af71
Oleg <oleg_09-95@mail.ru>
2026-03-30 21:47:24 +0300

    React 58 Добавлены Различные способы стилизации ссылок.

--- react/04-router/src/components/NavbarNavlink.jsx
+++ react/04-router/src/components/NavbarNavlink.jsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router";
 
 function NavbarNavlink(props) {
     return (
-        <nav>
+        <nav className="flex justify-between gap-1 w-1/3">
             <NavLink to={"/"} style={({isActive}) => ({
                 color: isActive ? 'green' : 'blue'
             })}>Главная</NavLink>
@@ -13,8 +13,10 @@ function NavbarNavlink(props) {
             <NavLink to={"/auth"} style={({isActive}) => ({
                 color: isActive ? 'tomato' : 'blue'
             })}>Войти</NavLink>
-            <NavLink to={""}>На главную</NavLink>
-            <NavLink to={""}>О нас</NavLink>
+            <NavLink to={"/"}>На главную</NavLink>
+            <NavLink to={"/about"} className={({isActive}) => (
+                isActive ? "text-green-800" : "text-shadow-green-200"
+            )}>О нас</NavLink>
         </nav>
     );
 }
