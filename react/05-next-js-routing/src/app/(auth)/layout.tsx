ea4f7a0d183bcd277f3552de984f49fdc507388e
Oleg <oleg_09-95@mail.ru>
2026-04-02 20:02:28 +0300

    Next.js 69 Рассмотрены темы: Поправил путь к общим стилям. Стилизовал auth layout

--- react/05-next-js-routing/src/app/(auth)/layout.tsx
+++ react/05-next-js-routing/src/app/(auth)/layout.tsx
@@ -1,11 +1,21 @@
+import "../globals.css";
+
 export default function AuthLayout({children,}: { children: React.ReactNode; }) {
   return (
       <html>
         <body className="min-h-screen flex flex-col">
-          <div className="flex-1 bg-amber-600">
+        <header className="flex justify-center p-5 ">Добро пожаловать</header>
+          <div className="flex-1 bg-gray-700">
             {children}
           </div>
-        <footer>Footer для страниц аутентификации</footer>
+        <footer className="p-10">
+            <div className="flex justify-around">
+                <div>О нас</div>
+                <div>Политика</div>
+                <div>Документация</div>
+                <div>Контакты</div>
+            </div>
+        </footer>
         </body>
       </html>
   );
