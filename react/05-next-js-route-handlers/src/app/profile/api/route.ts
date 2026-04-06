285691c99a076423b231dd1feb768eafe781425b
Oleg <oleg_09-95@mail.ru>
2026-04-06 20:09:51 +0300

    Next.js 79 Рассмотрены темы: Headers в Route Handlers В общем: ✅ Чтение заголовков запроса (Authorization, Accept, User-Agent) ✅ Установка заголовков ответа (Content-Type) ✅ Практические примеры для аутентификации и мобильных приложений

--- react/05-next-js-route-handlers/src/app/profile/api/route.ts
+++ react/05-next-js-route-handlers/src/app/profile/api/route.ts
@@ -9,15 +9,48 @@
 //     return new Response('📡-Route. From API in profile');
 // }
 
+// import {headers} from "next/headers";
+//
+// export async function GET() {
+//     const headerList = await headers();
+//     console.log(headerList.get('Authorization'));
+//
+//     return new Response('<h1>📡-Route. Headers</h1>', {
+//         headers: {
+//             "Content-Type": "text/html; charset=UTF-8"
+//         }
+//     });
+// }
+
 import {headers} from "next/headers";
 
 export async function GET() {
-    const headerList = await headers();
-    console.log(headerList.get('Authorization'));
+    const headerList = headers();
+    const acceptHeader = (await headerList).get('Accept');
+    const userData = {
+        name: 'Имя Именов',
+        email: 'Почта',
+        role: 'admin'
+    };
 
-    return new Response('<h1>📡-Route. Headers</h1>', {
+    if (acceptHeader?.includes('application/json')) {
+        return new Response(JSON.stringify(userData), {
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        })
+    }
+
+    return new Response(`
+        <h1>Профиль пользователя</h1>
+        <ul>
+            <li>Имя: ${userData.name}</li>
+            <li>Email: ${userData.email}</li>
+            <li>Роль: ${userData.role}</li>
+        </ul>
+    `, {
         headers: {
             "Content-Type": "text/html; charset=UTF-8"
         }
-    });
+    })
 }
