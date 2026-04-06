140303964ed694fa47bd7d847155b7f93560db7e
Oleg <oleg_09-95@mail.ru>
2026-04-06 19:39:53 +0300

    Next.js 79 Рассмотрены темы: Headers в Route Handlers Первый способ получения Headers

--- react/05-next-js-route-handlers/src/app/profile/api/route.ts
+++ react/05-next-js-route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,10 @@
-export async function GET() {
+import {type NextRequest} from 'next/server';
+
+export async function GET(request: NextRequest) {
+
+    const requestHeaders = new Headers(request.headers);
+    console.log(requestHeaders.get('Authorization'));
+
+
     return new Response('📡-Route. From API in profile');
 }
