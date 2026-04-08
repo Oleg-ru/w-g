7bff772fa0fb2d02f78f5e761561923e53034e85
Oleg <oleg_09-95@mail.ru>
2026-04-08 21:39:44 +0300

    Next.js 92 !!! полностью скопирован репо т.к не получилось настроить по курсу Рассмотрены темы: ✔ 🔧 Настройка Prisma в Next.js-проекте ✔ 💾 Подключение SQLite в качестве базы данных ✔ 📝 Создание модели Product в Prisma Schema ✔ 🛠 Миграции и отправка данных в БД ✔ ➕ Добавление продукта через Prisma Client

---
+++ react/06-next-js-fullstack/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Мое приложение на Next.js",
+    template: "%s | Мое приложение на Next.js",
+  },
+  description: "Я изучаю Next.js последней версии",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen flex flex-col bg-gray-50 dark:bg-gray-800 dark:text-white`}
+      >
+        <main className="p-8">{children}</main>
+      </body>
+    </html>
+  );
+}
