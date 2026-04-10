6ee9dcd667861f20a327a93b2b6d9180841fedd6
Oleg <oleg_09-95@mail.ru>
2026-04-10 19:09:30 +0300

    Next.js 99 Аутентификация в Next.js с Clerk

---
+++ react/07-next-js-clerk/src/components/Navigation.tsx
@@ -0,0 +1,38 @@
+import {
+    Show,
+    SignInButton,
+    SignOutButton, UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+
+export const Navigation = () => {
+    return (
+        <nav className="bg-background border-b border-(--foreground)/10">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16 items-center">
+                    <div className="shrink-0">
+                        <h1 className="text-xl font-semibold text-foreground">
+                            Мое приложение на Next.js
+                        </h1>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <Show when="signed-out">
+                            <SignInButton mode="modal">
+                                <>Войти</>
+                            </SignInButton>
+                        </Show>
+
+                        <UserButton />
+
+                        <Show when="signed-in">
+                            <Link href="/user-profile">Профиль</Link>
+                            <SignOutButton>
+                                <>Выйти</>
+                            </SignOutButton>
+                        </Show>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
---
+++ react/07-next-js-clerk/src/proxy.ts
@@ -0,0 +1,28 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+//Защита только указанных маршрутов от несанк. доступа
+// const isProtectedRoute = createRouteMatcher(['/user-profile']);
+// export default clerkMiddleware(async (auth, req) => {
+//     if (isProtectedRoute(req)) {
+//         await auth.protect()
+//     }
+// });
+
+//Защита всех маршрутов по умолчанию, а перечисляем к каким не нужна
+const isPublicRoute = createRouteMatcher(['/', '/signin(.*)', '/signup(.*)',]);
+export default clerkMiddleware(async (auth, req) => {
+    if (!isPublicRoute(req)) {
+        await auth.protect()
+    }
+});
+
+
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+};
