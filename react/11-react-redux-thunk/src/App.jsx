e82b3e21f036826b7e5b9b8fc0208271917a1f17
Oleg <oleg_09-95@mail.ru>
2026-04-18 15:32:42 +0300

    React Redux Toolkit 106 Стилизован TodoList

--- react/11-react-redux-thunk/src/App.jsx
+++ react/11-react-redux-thunk/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import AddTodo from "./components/AddTodo.jsx";
+import TodoList from "./components/TodoList.jsx";
 
 function App() {
   return (
     <>
-      <div className='flex justify-center items-center pt-4'>
+      <div className='flex justify-center flex-col items-center pt-4'>
         <AddTodo />
+        <TodoList />
       </div>
     </>
   )
--- react/11-react-redux-thunk/src/components/TodoList.jsx
+++ react/11-react-redux-thunk/src/components/TodoList.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 function TodoList(props) {
     return (
-        <div>
-            <input type="checkbox"/>
-            <span>Задача</span>
-            <button>❌</button>
+        <div className='w-1/2'>
+            <div className="flex justify-between gap-3 mt-5 p-2 border border-orange-500 rounded">
+                <input type="checkbox"/>
+                <span>Задача</span>
+                <button className="hover:bg-red-300 delay-75 rounded">❌</button>
+            </div>
         </div>
     );
 }
