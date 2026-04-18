3376613a952bb99ae7f9b7d2121eac77ce50fc9a
Oleg <oleg_09-95@mail.ru>
2026-04-18 22:31:24 +0300

    React+NextJS Интернет-магазин 1 Создан проект и настроен шрифт Rubik

---
+++ react/13-next-js-online-store/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+
+const rubik = Rubik({
+  variable: "--font-rubik",
+  subsets: ["latin", "cyrillic"],
+});
+
+export const metadata: Metadata = {
+  title: "Северяночка",
+  description: "Доставка и покупка продуктов питания",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${rubik.variable} font-sans h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
