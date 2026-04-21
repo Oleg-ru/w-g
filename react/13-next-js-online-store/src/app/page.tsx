9f5f91a91ece2efcd67deeaa8bbddb81692de383
Oleg <oleg_09-95@mail.ru>
2026-04-21 18:52:18 +0300

    React+NextJS Интернет-магазин 4 Акции

--- react/13-next-js-online-store/src/app/page.tsx
+++ react/13-next-js-online-store/src/app/page.tsx
@@ -1,9 +1,14 @@
 import Slider from "@/components/slider/Slider";
+import ProductCard from "@/components/ProductCard";
+import Actions from "@/components/Actions";
 
 export default function Home() {
   return (
     <main className="w-full mx-auto mb-20">
       <Slider />
+        <div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
+            <Actions />
+        </div>
     </main>
   );
 }
