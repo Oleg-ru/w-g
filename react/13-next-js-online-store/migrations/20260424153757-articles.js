71b56b094a752dc734f0f5d616fff0484f2234b2
Oleg <oleg_09-95@mail.ru>
2026-04-24 19:39:57 +0300

    React+NextJS Интернет-магазин 10 Миграция данных в MongoDB

---
+++ react/13-next-js-online-store/migrations/20260424153757-articles.js
@@ -0,0 +1,21 @@
+const articles = require('./articlesDatabase.json');
+
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+    await db.collection('articles').insertMany(articles)
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+
+  }
+};
---
+++ react/13-next-js-online-store/migrations/20260424160634-users.js
@@ -0,0 +1,11 @@
+const users = require('./usersDatabase.json')
+
+module.exports = {
+  async up(db) {
+    await db.collection('users').insertMany(users)
+  },
+
+  async down(db) {
+
+  }
+};
